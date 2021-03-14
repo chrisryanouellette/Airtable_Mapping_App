c9b6e090a68d1ae3ce2583ee284e9b7f94090863
Chris <chrisryanouellette@gmail.com>
2021-03-14 16:20:06 -0400

    Fix: default file name is remote.json

--- scripts/add_block.js
+++ scripts/add_block.js
@@ -11,15 +11,22 @@ const {
 
 const options = []
 
+/** Asks the user what to name the file.
+ * A blank response will use the defualt file name 'remote.json'
+ */
 function requestName() {
-	const question = 'What is the name of the base this blocks is running in? Ex, development'
+	const question = 'What is the name of the base this blocks is running in?\nNo response will create the default file called "remote.json"'
 	query(question, (response) => {
-        if(!response) response = 'development'
-		options[0] = response
+        if(!response) {
+            options[0] = ''
+        } else {
+            options[0] = response
+        }
         runProcess()
     })
 }
 
+/** Asks the user for the Base and App ID */
 function requestId() {
 	let question = 'What is the Base ID & Block ID? Ex, app92ce2v332v/blk23f32fhf2\n'
 	const fileNames = getFilesNames('.block/')
@@ -47,13 +54,15 @@ function requestId() {
     })
 }
 
+/** Creates the file */
 function createBlockFile() {
 	const data = {
 		baseId: options[1],
 		blockId: options[2]
 	}
+    const fileName = options[0] ? options[0] + '.remote.json' : 'remote.json'
 	fs.writeFileSync(
-		path.join(__dirname, '../.block/', options[0] + '.remote.json'),
+		path.join(__dirname, '../.block/', fileName),
 		JSON.stringify(data, undefined, 4)
 	)
 	process.exit(0)
